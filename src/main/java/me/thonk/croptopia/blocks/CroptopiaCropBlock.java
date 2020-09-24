d193753a132f90dfcf22e43184c14c9e7645acaa
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2020-09-23 21:23:44 -0500

    add rest of blockstates for crop stages and begin texturing some crops

--- src/main/java/me/thonk/croptopia/blocks/CroptopiaCropBlock.java
+++ src/main/java/me/thonk/croptopia/blocks/CroptopiaCropBlock.java
@@ -1,18 +1,40 @@
 package me.thonk.croptopia.blocks;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 
 public class CroptopiaCropBlock extends CropBlock {
+    private static final VoxelShape[] AGE_TO_SHAPE = new VoxelShape[]{
+            Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 2.0D, 16.0D),
+            Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 3.0D, 16.0D),
+            Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 4.0D, 16.0D),
+            Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 5.0D, 16.0D),
+            Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 6.0D, 16.0D),
+            Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 7.0D, 16.0D),
+            Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 8.0D, 16.0D),
+            Block.createCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 9.0D, 16.0D)};
 
     private final Item seed;
 
     protected CroptopiaCropBlock(Settings settings, Item seed) {
         super(settings);
         this.seed = seed;
+        ItemStack
     }
 
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return AGE_TO_SHAPE[state.get(this.getAgeProperty())];
+    }
 
     @Override
     protected ItemConvertible getSeedsItem() {
