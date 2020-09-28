de177116489a766469541735f9756a8086e8766b
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2020-09-28 01:04:16 -0500

    add more crops. change some crops to use a cross pattern instead of the 4 planes

--- src/main/java/me/thonk/croptopia/blocks/CroptopiaCropBlock.java
+++ src/main/java/me/thonk/croptopia/blocks/CroptopiaCropBlock.java
@@ -4,11 +4,17 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class CroptopiaCropBlock extends CropBlock {
     private static final VoxelShape[] AGE_TO_SHAPE = new VoxelShape[]{
@@ -35,6 +41,21 @@ public class CroptopiaCropBlock extends CropBlock {
 
     public void setSeedsItem(Item seed) {
         this.seed = seed;
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (getAge(state) == getMaxAge()) {
+            world.setBlockState(pos, this.withAge(0), 2);
+            dropStacks(state, world, pos);
+            return ActionResult.CONSUME;
+        }
+        return ActionResult.PASS;
+    }
+
+    @Override
+    public void onLandedUpon(World world, BlockPos pos, Entity entity, float distance) {
+        super.onLandedUpon(world, pos, entity, distance);
     }
 
     @Override
