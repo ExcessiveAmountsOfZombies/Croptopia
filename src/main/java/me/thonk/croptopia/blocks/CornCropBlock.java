c7f09752075ad65c872ed1e71be686964709e495
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2020-09-28 17:42:19 -0500

    center the corn plant bounding box

--- src/main/java/me/thonk/croptopia/blocks/CornCropBlock.java
+++ src/main/java/me/thonk/croptopia/blocks/CornCropBlock.java
@@ -5,7 +5,6 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.item.Item;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 
@@ -27,9 +26,7 @@ public class CornCropBlock extends CroptopiaCropBlock {
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        Vec3d vec3d = state.getModelOffset(world, pos);
-        VoxelShape shape = AGE_TO_SHAPE[state.get(this.getAgeProperty())];
-        return shape.offset(vec3d.x, vec3d.y, vec3d.z);
+        return AGE_TO_SHAPE[state.get(this.getAgeProperty())];
     }
 
     @Override
