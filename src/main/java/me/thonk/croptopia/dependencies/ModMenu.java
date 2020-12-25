6485e4211bf027bdd620c2b3bdb0297b221a4e71
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2020-12-25 17:57:31 -0600

    add ModMenu compatibility

---
+++ src/main/java/me/thonk/croptopia/dependencies/ModMenu.java
@@ -0,0 +1,21 @@
+package me.thonk.croptopia.dependencies;
+
+import io.github.prospector.modmenu.api.ModMenuApi;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+
+@Environment(EnvType.CLIENT)
+public class ModMenu extends Dependency implements ModMenuApi {
+
+    public ModMenu() {
+        super("modmenu");
+    }
+
+    @Override
+    public String getModId() {
+        return "Croptopia";
+    }
+
+
+}
