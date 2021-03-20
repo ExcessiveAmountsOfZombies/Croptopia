c0f925a276b88e3abbcd671f577ff20262a2db29
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2021-03-20 15:41:08 -0500

    accessor mixin is apparently good enough

--- fabric/src/main/java/me/thonk/croptopia/mixin/AxeMixin.java
+++ fabric/src/main/java/me/thonk/croptopia/mixin/AxeMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Slice;
