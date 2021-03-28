87b1e520803d5741bd04061f7553bba3163828aa
Breki Tomasson <breki.tomasson@gmail.com>
2021-03-28 12:42:53 +0200

    Added `Croptopia.cropItems` to the list of compostable things.

--- fabric/src/main/java/me/thonk/croptopia/registry/Composter.java
+++ fabric/src/main/java/me/thonk/croptopia/registry/Composter.java
@@ -8,6 +8,9 @@ import static net.minecraft.block.ComposterBlock.ITEM_TO_LEVEL_INCREASE_CHANCE;
 public class Composter {
 
     public static void init() {
+        Croptopia.cropItems.forEach(cropItem -> {
+            registerCompostableItem(0.65F, cropItem);
+        });
         Croptopia.getSeeds().forEach(configurableSeed -> {
             registerCompostableItem(0.3F, configurableSeed.getSeedItem());
         });
