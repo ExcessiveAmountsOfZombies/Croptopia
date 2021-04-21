a3b000398ee38432de13c5261623d72602c75957
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-04-21 23:50:56 +0200

    fix seeds planted by villagers not being removed from their inventory

--- fabric/src/main/java/me/thonk/croptopia/mixin/FarmerVillagerTaskMixin.java
+++ fabric/src/main/java/me/thonk/croptopia/mixin/FarmerVillagerTaskMixin.java
@@ -12,27 +12,38 @@ import net.minecraft.util.math.BlockPos;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(FarmerVillagerTask.class)
 public class FarmerVillagerTaskMixin {
 
-
     @Shadow @Nullable private BlockPos currentTarget;
+    @Unique boolean planted = false;
 
     @Inject(method = "keepRunning", at = @At(value = "INVOKE_ASSIGN",
             target = "Lnet/minecraft/item/ItemStack;isEmpty()Z", opcode = 0),
             locals = LocalCapture.CAPTURE_FAILEXCEPTION)
     public void running(ServerWorld serverWorld, VillagerEntity villagerEntity, long l, CallbackInfo ci,
                         BlockState blockState, Block block, Block block2,
-                        SimpleInventory inventory, int i, ItemStack itemStack, boolean bl) {
+                        SimpleInventory inventory, int i, ItemStack itemStack) {
         if (itemStack.getItem() instanceof SeedItem) {
             serverWorld.setBlockState(this.currentTarget, ((SeedItem) itemStack.getItem()).getBlock().getDefaultState(), 3);
-            bl = true;
+            planted = true;
+        }
+    }
+
+    @ModifyVariable(method = "keepRunning", at = @At(value = "LOAD", ordinal = 0))
+    public boolean planted(boolean bl) {
+        if (planted) {
+            planted = false;
+            return true;
         }
 
+        return bl;
     }
 }
