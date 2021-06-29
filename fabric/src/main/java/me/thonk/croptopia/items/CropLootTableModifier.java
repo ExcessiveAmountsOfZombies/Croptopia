af345399478dc6735df2861cdb408a5889fe5bdb
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2021-06-28 20:19:23 -0500

    Add croptopia seeds to bonus chest

--- fabric/src/main/java/me/thonk/croptopia/items/CropLootTableModifier.java
+++ fabric/src/main/java/me/thonk/croptopia/items/CropLootTableModifier.java
@@ -7,15 +7,16 @@ import me.thonk.croptopia.loottables.BiomeLootCondition;
 import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
-import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.loot.LootManager;
 import net.minecraft.loot.condition.MatchToolLootCondition;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.AlternativeEntry;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.ApplyBonusLootFunction;
-import net.minecraft.loot.function.LootFunction;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.predicate.item.ItemPredicate;
 
@@ -24,11 +25,11 @@ import java.util.ArrayList;
 public class CropLootTableModifier {
 
     public static void init() {
-        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, identifier, fabricLootSupplierBuilder, lootTableSetter) -> {
-            if (identifier.getPath().equalsIgnoreCase("blocks/grass") ||
-                    identifier.getPath().equalsIgnoreCase("blocks/tall_grass") ||
-                    identifier.getPath().equalsIgnoreCase("blocks/fern") ||
-                    identifier.getPath().equalsIgnoreCase("blocks/large_fern")) {
+        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, fabricLootSupplierBuilder, lootTableSetter) -> {
+            if (id.getPath().equalsIgnoreCase("blocks/grass") ||
+                    id.getPath().equalsIgnoreCase("blocks/tall_grass") ||
+                    id.getPath().equalsIgnoreCase("blocks/fern") ||
+                    id.getPath().equalsIgnoreCase("blocks/large_fern")) {
                 FabricLootPoolBuilder builder = FabricLootPoolBuilder.builder();
                 builder.rolls(UniformLootNumberProvider.create(0, 1));
                 builder.withFunction(ApplyBonusLootFunction.oreDrops(Enchantments.FORTUNE).build());
@@ -46,14 +47,22 @@ public class CropLootTableModifier {
                 fabricLootSupplierBuilder.withPool(builder.build());
                 //System.out.println(LootManager.toJson(fabricLootSupplierBuilder.build()));
             }
-        });
-    }
 
-    public static void addToBonusChest() {
-        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, identifier, fabricLootSupplierBuilder, lootTableSetter) -> {
-            if (identifier.getPath().equalsIgnoreCase("")) {
-
+            if (id.getNamespace().equalsIgnoreCase("minecraft") && id.getPath().equalsIgnoreCase("chests/spawn_bonus_chest")) {
+                FabricLootPoolBuilder builder = FabricLootPoolBuilder.builder();
+                builder.rolls(ConstantLootNumberProvider.create(1));
+                builder.bonusRolls(ConstantLootNumberProvider.create(0));
+                for (ConfigurableSeed seed : Croptopia.getSeeds()) {
+                    builder.withEntry(
+                            ItemEntry.builder(seed.getSeedItem())
+                                    .weight(5)
+                                    .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(3, 8), false)).build()
+                    );
+                }
+                fabricLootSupplierBuilder.pool(builder);
+                System.out.println(LootManager.toJson(fabricLootSupplierBuilder.build()));
             }
+
         });
     }
 }
