c91cb760e6c675759278b01fa67e006d3e8398ee
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2021-12-13 14:11:41 -0600

    prepare for patchouli to be updated to 1.18

--- forge/src/main/java/me/thonk/croptopia/items/GuideBookItem.java
+++ forge/src/main/java/me/thonk/croptopia/items/GuideBookItem.java
@@ -12,7 +12,7 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.registries.ForgeRegistries;
-//import vazkii.patchouli.api.PatchouliAPI;
+import vazkii.patchouli.api.PatchouliAPI;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -30,7 +30,7 @@ public class GuideBookItem extends Item {
         ItemStack stack = user.getItemInHand(hand);
 
         if (user instanceof ServerPlayer player && ModList.get().isLoaded("patchouli")) {
-            //PatchouliAPI.get().openBookGUI(player, ForgeRegistries.ITEMS.getKey(this));
+            PatchouliAPI.get().openBookGUI(player, ForgeRegistries.ITEMS.getKey(this));
         }
 
         return new InteractionResultHolder<>(InteractionResult.SUCCESS, stack);
@@ -40,8 +40,8 @@ public class GuideBookItem extends Item {
     public void appendHoverText(ItemStack p_41421_, @Nullable Level p_41422_, List<Component> tooltip, TooltipFlag p_41424_) {
         if (!ModList.get().isLoaded("patchouli")) {
             tooltip.add(Component.nullToEmpty("Patchouli is not installed."));
-        } else {
+        } /*else {
             tooltip.add(Component.nullToEmpty("let me know patchouli is updated either in my discord or in a Github Issue!"));
-        }
+        }*/
     }
 }
