06f7c94476390ee022b46117eba36872c5aab14f
Thonk <30448663+ExcessiveAmountsOfZombies@users.noreply.github.com>
2022-01-01 15:27:51 -0600

    Fix seeds dropping in creative mode #289

--- forge/src/main/java/me/thonk/croptopia/events/BlockBreakEvent.java
+++ forge/src/main/java/me/thonk/croptopia/events/BlockBreakEvent.java
@@ -1,23 +1,13 @@
 package me.thonk.croptopia.events;
 
-import me.thonk.croptopia.CroptopiaForge;
-import me.thonk.croptopia.items.SeedItem;
 import me.thonk.croptopia.registry.BlockRegistry;
 import me.thonk.croptopia.registry.ItemRegistry;
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.storage.loot.LootContext;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-import java.util.List;
 import java.util.Random;
 
 public class BlockBreakEvent {
@@ -29,39 +19,14 @@ public class BlockBreakEvent {
     public void onInteractionWithTool(BlockEvent.BlockToolInteractEvent event) {
         BlockState state = event.getState();
         if (state.is(BlockRegistry.cinnamonLog) || state.is(BlockRegistry.cinnamonWood)) {
-            Block.popResource(event.getPlayer().level, event.getPos(), new ItemStack(ItemRegistry.cinnamon));
+            if (!event.getPlayer().isCreative()) {
+                Block.popResource(event.getPlayer().level, event.getPos(), new ItemStack(ItemRegistry.cinnamon));
+            }
             if (state.is(BlockRegistry.cinnamonLog)) {
                 event.setFinalState(BlockRegistry.strippedCinnamonLog.withPropertiesOf(state));
             } else {
                 event.setFinalState(BlockRegistry.strippedCinnamonWood.withPropertiesOf(state));
             }
         }
-    }
-
-    @SubscribeEvent
-    public void onBlockBreak(BlockEvent.BreakEvent event) {
-        if (event.getState().is(Blocks.GRASS) || event.getState().is(Blocks.TALL_GRASS) ||
-            event.getState().is(Blocks.FERN)  || event.getState().is(Blocks.LARGE_FERN)) {
-            if (event.getWorld() instanceof ServerLevel) {
-                ServerLevel serverWorld = (ServerLevel) event.getWorld();
-                BlockPos pos = event.getPos();
-                // todo: figure out why i was doing this?
-                List<ItemStack> stack = event.getState().getDrops((new LootContext.Builder(serverWorld))
-                        .withRandom(serverWorld.random)
-                        .withParameter(LootContextParams.ORIGIN, Vec3.atCenterOf(pos))
-                        .withParameter(LootContextParams.TOOL, ItemStack.EMPTY));
-
-                Biome biome = event.getWorld().getBiome(event.getPos());
-                for (SeedItem seed : CroptopiaForge.seeds) {
-                    float value = random.nextFloat();
-                    if (biome.getBiomeCategory().equals(seed.getCategory())) {
-                        if (value > 0.99f) {
-                            Block.popResource(serverWorld, pos, new ItemStack(seed.asItem()));
-                        }
-                    }
-                }
-            }
-        }
-
     }
 }
