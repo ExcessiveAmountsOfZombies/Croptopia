935ad80648a6c087e39a12003da3ce5b6f8c9246
iAmThonk <30448663+excessiveamountsofzombies@users.noreply.github.com>
2026-09-17 21:16:58 -0500

    Re-add patchouli

--- fabric/src/main/java/com/epherical/croptopia/GuideBookItem.java
+++ fabric/src/main/java/com/epherical/croptopia/GuideBookItem.java
@@ -1,6 +1,7 @@
 package com.epherical.croptopia;
 
 import com.epherical.croptopia.platform.Services;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
@@ -8,6 +9,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
+import vazkii.patchouli.api.PatchouliAPI;
 
 public class GuideBookItem extends Item {
 
@@ -19,7 +21,7 @@ public class GuideBookItem extends Item {
     public InteractionResult use(Level world, Player user, InteractionHand hand) {
 
         if (user instanceof ServerPlayer player && Services.PLATFORM.isModLoaded("patchouli")) {
-            //PatchouliAPI.get().openBookGUI(player, BuiltInRegistries.ITEM.getKey(this));
+            PatchouliAPI.get().openBookGUI(player, BuiltInRegistries.ITEM.getKey(this));
             player.sendSystemMessage(Component.nullToEmpty("Sorry, As of the publishing date of this build, Patchouli isn't available for 26.1!" +
                     " When it's available ask croptopia dev (me) for an update."));
         }
--- neoforge/src/main/java/com/epherical/croptopia/GuideBookItem.java
+++ neoforge/src/main/java/com/epherical/croptopia/GuideBookItem.java
@@ -1,5 +1,6 @@
 package com.epherical.croptopia;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
@@ -8,6 +9,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.neoforged.fml.ModList;
+import vazkii.patchouli.api.PatchouliAPI;
 
 public class GuideBookItem extends Item {
 
@@ -22,7 +24,7 @@ public class GuideBookItem extends Item {
         if (user instanceof ServerPlayer player && ModList.get().isLoaded("patchouli")) {
             player.sendSystemMessage(Component.nullToEmpty("Sorry, As of the publishing date of this build, Patchouli isn't available for 26.1!" +
                     " When it's available ask croptopia dev (me) for an update."));
-            //PatchouliAPI.get().openBookGUI(player, BuiltInRegistries.ITEM.getKey(this));
+            PatchouliAPI.get().openBookGUI(player, BuiltInRegistries.ITEM.getKey(this));
         }
 
         return InteractionResult.PASS;
